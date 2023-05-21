ec69b657a73bba3ba4e5fa12d98ef117fba55d7f
Nakul Bansal <nakulbansal727@gmail.com>
2023-05-20 17:09:52 -0700

    Added new like button.

--- src/Post.js
+++ src/Post.js
@@ -1,18 +1,21 @@
 import "./Post.css";
 import "./buttons.css";
 
-// props:
-// image; title; author; date
 export default function Post(props) {
-	return (
-		<div className="post">
-			<img src={props.image} alt={props.title} />
+  return (
+    <div className="post">
+      <img src={props.image} alt={props.title} />
+	  <div className="post-description">
+		<div>
 			<h1>{props.title}</h1>
 			<h2>{props.author}</h2>
 			<h3>{props.date}</h3>
-			<div className="">
-				<button>button</button>
-			</div>
 		</div>
-	);
+		<div className="like_button">
+			<button><img src="images/thumbsup_opaque.png"/></button>
+		</div>
+		
+	  </div>
+    </div>
+  );
 }
