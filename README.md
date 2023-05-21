6bafaec31d3bf881aae990a6f8618d3b6b74fff1
Micah Baker <micahb923@gmail.com>
2023-05-20 20:35:02 -0700

    Implemented backend

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# StormHacks 2023 Project
+# Glyptodon
+
+## Go API
+
+The following are for HTTP GETs:
+
+`/api/posts` returns all posts visible to the user.
+`/api/post/<PID>` returns the information associated with a specific post.
+`/api/comments/<PID>` returns all comments on a post
+
+`/api/users.json` returns all users known to the user.
+`/api/user/<UID>` returns the information associated with a specific user.
+
+The following are for HTTP POSTs:
+
+`/api/create`
+
+Which expects JSON with at least the following field set:
+
+```json
+"Creating": "Post" / "Comment" / "User"
+```
+
+When you are creating a post, the following must be posted:
+
+```json
+{
+    "Creating": "Post",
+    "User": "UID (Pre-existing)"
+    "Image": "Image URL",
+    "Title": "Post Title",
+    "Text": "This is what this post is about!"
+}
+```
+
+When you are creating a comment, the following must be posted:
+
+```json
+{
+    "Creating": "Comment",
+    "Post": "PID (Pre-existing)",
+    "User": "UID (Pre-existing)",
+    "Text": "This is the comment itself."
+}
+
+When you are creating a user, the following must be posted:
+
+```json
+{
+    "Creating": "User",
+    "Name": "John Doe",
+    "Image": "Image URL",
+}
+```
---
+++ server/server.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+)
+
+const (
+	DOG1 = "https://random.dog/1326984c-39b0-492c-a773-f120d747a7e2.jpg"
+	DOG2 = "https://random.dog/1abd3cbd-d6db-435b-9218-a9b6a26be50b.JPG"
+	DOG3 = "https://random.dog/5d6792a5-a458-4077-b9ee-de7f15706cca.jpg"
+	DOG4 = "https://random.dog/c8b7a017-8966-4f84-b2c6-609a739d833e.jpg"
+	DOG5 = "https://random.dog/831d264b-01c6-41f2-a00a-c798c9b72e7e.jpg"
+	DOG6 = "https://random.dog/9efb1b9f-389e-4bd6-a67a-e25cb166aa91.gif"
+	DOG7 = "https://random.dog/2b77b03c-3073-454e-957b-867580b3d005.jpg"
+	DOG8 = "https://random.dog/b775c9ae-205a-47ad-96e5-bea4d78628c5.jpg"
+)
+
+type User struct {
+	UID int
+	Name string
+	Image string
+	Date string
+}
+
+type Comment struct {
+	UID int
+	Comment string
+}
+
+type Post struct {
+	PID int
+	UID int
+	Image string
+	Title string
+	Text string
+	Comments []Comment
+	Date string
+}
+
+var user []User
+var post []Post
+
+func init() {
+	user = append(user, User{0, "Micah Baker", DOG1, "Today"})  // 0
+	user = append(user, User{1, "Nakul Bansal", DOG2, "Today"}) // 1
+	user = append(user, User{2, "Simon Purdon", DOG3, "Today"}) // 2
+
+	post = append(post, Post{0, 0, DOG4, "New Dog", "Cute dog", nil, "Today"})
+	post = append(post, Post{1, 1, DOG5, "Hello Dog", "Crazy dog", nil, "Today"})
+	post = append(post, Post{2, 2, DOG6, "No more Dog", "Weird dog", nil, "Today"})
+}
+
+func main() {
+	e := echo.New()
+
+	e.GET("/", notFound)
+	e.GET("/api/posts", getPosts)
+	e.GET("/api/post/:PID", getPost)
+	e.GET("/api/comments/:PID", getComments)
+	e.GET("/api/users", getUsers)
+	e.GET("/api/user/:UID", getUser)
+
+	fmt.Print("Starting Glyptodon Go Backend...")
+
+	log.Fatal(e.Start(":8000"))
+}
+
+func notFound(c echo.Context) error {
+	return echo.NewHTTPError(http.StatusNotFound)
+}
+
+func getPosts(c echo.Context) error {
+	return c.JSON(http.StatusOK, post)
+}
+
+func getPost(c echo.Context) error {
+	i, err := strconv.Atoi(c.Param("PID"))
+	if err != nil || i < 0 || i >= len(post) {
+		return echo.NewHTTPError(http.StatusNotFound)
+	}
+	return c.JSON(http.StatusOK, post[i])
+}
+
+func getComments(c echo.Context) error {
+	i, err := strconv.Atoi(c.Param("PID"))
+	if err != nil || i < 0 || i >= len(post) {
+		return echo.NewHTTPError(http.StatusNotFound)
+	}
+	return c.JSON(http.StatusOK, post[i].Comments)
+}
+
+func getUsers(c echo.Context) error {
+	return c.JSON(http.StatusOK, user)
+}
+
+func getUser(c echo.Context) error {
+	i, err := strconv.Atoi(c.Param("UID"))
+	if err != nil || i < 0 || i >= len(user) {
+		return echo.NewHTTPError(http.StatusNotFound)
+	}
+	return c.JSON(http.StatusOK, user[i])
+}
