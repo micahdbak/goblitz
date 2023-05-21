19b539f3ed16bba9a53b471edd7d43e2c7a0f854
Simon Purdon <simon.g.purdon@gmail.com>
2023-05-20 17:44:41 -0700

    Added some functionality to like button

--- src/Post.js
+++ src/Post.js
@@ -1,26 +1,38 @@
 import "./Post.css";
 import "./buttons.css";
+import React, {useState, useCallback} from 'react';
+
+const THUMBSUP = "images/thumbsup_opaque.png"; 
 
 export default function Post(props) {
-  return (
-    <div className="post">
-      <img src={props.image} alt={props.title} />
-	  <div className="post-description">
-		<div>
-			<h1>{props.title}</h1>
-			<h2>{props.author}</h2>
-			<h3>{props.date}</h3>
-			<div className="btn-row">
-				<button className="row-btn sm primary">1</button>
-				<button className="row-btn sm secondary">2</button>
-				<button className="row-btn sm secondary">3</button>
+	const [likeCount, setLikeCount] = useState(0);
+	const [liked, setLiked] = useState(0);
+	const handleClick = useCallback(() => {
+		if(liked == 0){
+			setLikeCount(likeCount + 1);
+			setLiked(1);
+		}
+		else {
+			setLikeCount(likeCount - 1);
+			setLiked(0);
+		}
+	});
+	return (
+		<div className="post">
+			<img src={props.image} alt={props.title} />
+				<div className="post-description">
+				<div>
+					<h1>{props.title}</h1>
+					<h2>{props.author}</h2>
+					<h3>{props.date}</h3>
+				</div>
+				<div className="like_button">
+					<button onClick={handleClick}>
+						<p>Likes: {likeCount}</p>
+						<img src={THUMBSUP} />
+					</button>
+				</div>
 			</div>
 		</div>
-		<div className="like_button">
-			<button><img src="images/thumbsup_opaque.png"/></button>
-		</div>
-		
-	  </div>
-    </div>
-  );
+	);
 }
